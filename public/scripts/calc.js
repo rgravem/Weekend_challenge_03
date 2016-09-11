ac4efafe8363c72c31ca10ed69e896b1bd2b4973
Ross Gravem <rgravem@yahoo.com>
2016-09-10 21:40:38 -0500

    operations and clear button work

--- public/scripts/calc.js
+++ public/scripts/calc.js
@@ -1,18 +1,39 @@
 console.log('calc.js sourced');
 
+
 $(document).ready(function(){
   console.log('document ready');
+
 
   $('#calculate').on('click', function(){
     var firstval = $('#value1').val();
     var secondval = $('#value2').val();
+    var operation = $('.operation').val();
 
-    $.ajax({
-      type: 'POST',
-      url: '/',
-      data: 
-    });
+    var objectToSend = {
+      x:Number(firstval),
+      y:Number(secondval),
+      operator: operation,
+    };
+    console.log(objectToSend);
+    var sendObject = function() {
+      $.ajax({
+        type: 'POST',
+        url: '/calculate',
+        data: objectToSend,
+        success: function(data){
+          console.log('client data:', data.result);
+          $('#results').append("Result: ", data.result);
+        }
+      });
+    };
+    sendObject();
+
+
   });
 
+  $('#clear').on('click', function(){
+    location.reload();
+  });
 
 });
--- server/app.js
+++ server/app.js
@@ -16,6 +16,39 @@ app.get('/', function(req, res){
 
 app.post('/calculate', urlencodedparser, function(req, res){
   console.log('serverpost calculate hit', req.body);
+  var x = Number(req.body.x);
+  var y = Number(req.body.y);
+  var oper = req.body.operator;
+
+  var doWork = function(){
+    if (oper == '+') {
+      var workAdd = x + y;
+      var sendAdd = {
+        result: workAdd,
+      };
+      res.send(sendAdd);
+    }else if (oper == '-') {
+      var workSub = x - y;
+      var sendSub = {
+        result: workSub,
+      };
+      res.send(sendSub);
+    }else if (oper == '*'){
+      var workMult = x * y;
+      var sendMult = {
+        result: workMult,
+      };
+      res.send(sendMult);
+    }else if (oper == '/'){
+      var workDiv = x / y;
+      var sendDiv = {
+        result: workDiv,
+      };
+      res.send(sendDiv);
+    }
+    console.log(res.send);
+  };
+  doWork();
 });
 
 app.use(express.static('public'));
